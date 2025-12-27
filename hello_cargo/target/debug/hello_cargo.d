0284154c8a589226068a74115910cf44a7fa3426
CheeseyHub <muhammedibrahim613@gmail.com>
2025-12-27 22:33:16 +0500

    learned about cargo

---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/ibrahim/projects/rust_learning/hello_cargo/target/debug/hello_cargo: /home/ibrahim/projects/rust_learning/hello_cargo/src/main.rs
---
+++ hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/ibrahim/projects/rust_learning/hello_cargo/target/release/hello_cargo: /home/ibrahim/projects/rust_learning/hello_cargo/src/main.rs
