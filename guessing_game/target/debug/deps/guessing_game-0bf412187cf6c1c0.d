82bf6389864c3600fb1d6028953b764400e42106
CheeseyHub <muhammedibrahim613@gmail.com>
2025-12-28 08:15:30 +0500

    Learned about input output in rust

---
+++ guessing_game/target/debug/deps/guessing_game-0bf412187cf6c1c0.d
@@ -0,0 +1,5 @@
+/home/ibrahim/projects/learning_rust/guessing_game/target/debug/deps/guessing_game-0bf412187cf6c1c0.d: src/main.rs
+
+/home/ibrahim/projects/learning_rust/guessing_game/target/debug/deps/libguessing_game-0bf412187cf6c1c0.rmeta: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/ibrahim/projects/learning_rust/guessing_game/target/debug/guessing_game: /home/ibrahim/projects/learning_rust/guessing_game/src/main.rs
